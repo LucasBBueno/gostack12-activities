b4244d8330db87b360e598faf70abbfad7cb1eb9
Lucas Bueno <lucas-jbb@hotmail.com>
2020-06-24 23:48:27 -0300

    Changes for use Repository Pattern and Service Pattern

--- Nivel02/primeiro-projeto-node/src/routes/index.ts
+++ Nivel02/primeiro-projeto-node/src/routes/index.ts
@@ -1,16 +1,9 @@
 import { Router } from 'express';
+import appointmentsRouter from './appointments.routes';
 
 const routes = Router();
 
-routes.post('/users', (request, response) => {
-  const { name, email } = request.body;
-
-  const user = {
-    name,
-    email,
-  };
-
-  return response.json(user);
-});
+// Toda rota que começar com appointments será direcionada para o appointmentsRouter
+routes.use('/appointments', appointmentsRouter);
 
 export default routes;
