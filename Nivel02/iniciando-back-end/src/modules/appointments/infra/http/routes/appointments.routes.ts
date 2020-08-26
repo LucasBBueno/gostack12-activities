46e66226e83f132be7abe526879ff922f7d1070f
Lucas Bueno <lucas-jbb@hotmail.com>
2020-08-26 11:14:17 -0300

    Update CreateAppointmentsService to use Repository as dependency

--- Nivel02/iniciando-back-end/src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ Nivel02/iniciando-back-end/src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -1,5 +1,4 @@
 import { Router } from 'express';
-import { getCustomRepository } from 'typeorm';
 import { parseISO } from 'date-fns';
 
 import AppointmentsRepository from '@modules/appointments/infra/typeorm/repositories/AppointmentsRepository';
@@ -8,25 +7,28 @@ import CreateAppointmentService from '@modules/appointments/services/CreateAppoi
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 
 const appointmentsRouter = Router();
+const appointmentsRepository = new AppointmentsRepository();
 
 // Aplicar o middlware para todas as rotas
 appointmentsRouter.use(ensureAuthenticated);
 
-appointmentsRouter.get('/', async (request, response) => {
-  console.log(request.user);
-  const appointmentsRepository = getCustomRepository(AppointmentsRepository);
+// appointmentsRouter.get('/', async (request, response) => {
+//   console.log(request.user);
+//   const appointmentsRepository = getCustomRepository(AppointmentsRepository);
 
-  const appointments = await appointmentsRepository.find();
+//   const appointments = await appointmentsRepository.find();
 
-  return response.json(appointments);
-});
+//   return response.json(appointments);
+// });
 
 appointmentsRouter.post('/', async (request, response) => {
   const { provider_id, date } = request.body;
 
   const parsedDate = parseISO(date);
 
-  const createAppointmentService = new CreateAppointmentService();
+  const createAppointmentService = new CreateAppointmentService(
+    appointmentsRepository,
+  );
 
   const appointment = await createAppointmentService.execute({
     provider_id,
